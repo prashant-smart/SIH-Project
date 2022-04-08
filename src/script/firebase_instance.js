ca57567d6dadeb43f876cdbc14c5ab66afff0afb
chanpreet3000 <chanpreet3000@users.noreply.github.com>
2022-04-08 15:01:07 +0530

    Added firebase

---
+++ src/script/firebase_instance.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCVlq18Ijk-1tm4FeOUkxyXGxHHDSOkzg",
+    authDomain: "hack-sih.firebaseapp.com",
+    databaseURL: "https://hack-sih-default-rtdb.firebaseio.com",
+    projectId: "hack-sih",
+    storageBucket: "hack-sih.appspot.com",
+    messagingSenderId: "1067640340751",
+    appId: "1:1067640340751:web:df5c5a929477def6b27e7a",
+    measurementId: "G-JVWJ9Q3K14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const reference = ref(db, 'api');
+
+onValue(reference, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+        //calling to fetch college data from api
+        fetchCollegeData(childSnapshot.val());
+    });
+});
+
+
+function fetchCollegeData(link) {
+    fetch(link)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        })
+        .catch(function(error) {
+            console.log('Looks like there was a problem: ', error);
+        });
+}
