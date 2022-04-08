bac6a5f202b483920d1aae41e52032cfb0917680
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-04-08 16:40:05 +0530

    fixed loader

--- src/script/firebase_instance.js
+++ src/script/firebase_instance.js
@@ -1,3 +1,6 @@
+let parent_container=document.getElementsByClassName('parent_container')[0];
+parent_container.style.display='none';
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
@@ -21,20 +24,19 @@ onValue(reference, (snapshot) => {
     snapshot.forEach((childSnapshot) => {
         //calling to fetch college data from api
         fetchCollegeData(childSnapshot.val());
-        while(college_data.size()){
-            
-        }
     });
 });
 
 function fetchCollegeData(link) {
+    let loader=document.getElementsByClassName('wrapper')[0];
     fetch(link)
     .then((response) => {
         return response.json();
     })
     .then((myJson) => {
+        parent_container.style.display='block';
+        loader.style.display='none';
         college_data=myJson;
-        console.log(college_data);
     })
     .catch(function(error) {
         console.log('Looks like there was a problem: ', error);
